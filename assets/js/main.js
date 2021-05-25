f4f4d9907d621e3f2e44caa21251fe80daf1bc53
Hiimus <hiimhanseneirik@gmail.com>
2021-05-25 14:20:21 +0000

    Added code to prevent the ability to click on more than two cards at once

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,14 @@
 const cards = document.querySelectorAll('.game-card');
 
+let lock = false;
 let firstSign, secondSign;
 let hasFlipped = false;
 
 
 function cardFlip() {
+    if (lock) return;
+    if (this === firstSign) return;
+
     this.classList.add('cardFlipped');
 
     // first click
@@ -13,30 +17,38 @@ function cardFlip() {
         firstSign = this;
     // second click    
     } else {
-        hasFlipped = false;
+        
         secondSign = this;
 
         if (firstSign.dataset.name === secondSign.dataset.name) {
             //It's a match
-            
             firstSign.removeEventListener('click', cardFlip);
             secondSign.removeEventListener('click', cardFlip);
             setTimeout(function () {
                 firstSign.style.transform = "rotateY(180deg)";
                 secondSign.style.transform = "rotateY(180deg)";
             }, 300);
+
+            reset();
             
         } else {
+          // Not a match  
+          lock = true;
           setTimeout(function() {
             firstSign.classList.remove('cardFlipped');
             secondSign.classList.remove('cardFlipped');
+            
+            reset();
           }, 1000);
             
         }    
     }
 }
 
-
+function reset() {
+    [hasFlipped, lock] = [false, false];
+    [firstSign, secondSign] = [null. null];
+}
 
 
 cards.forEach(board => board.addEventListener('click', cardFlip));
