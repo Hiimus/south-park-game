e6545bf91c6a18e75837a762587349df1883a54c
Hiimus <hiimhanseneirik@gmail.com>
2021-05-31 10:09:52 +0000

    Fixed card flipping after finding a pair

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
-$(document).ready(function(){
-        $("#exampleModal").modal('show');
-	});
+$(document).ready(function () {
+    cards.forEach(sign => sign.style.display = 'block');
+    $("#exampleModal").modal('show');
+});
 
 const cards = document.querySelectorAll('.game-card');
 const cardsMd = document.querySelectorAll('.game-card, .game-card-md');
@@ -11,7 +12,7 @@ let lock = false;
 let firstSign, secondSign;
 let hasFlipped = false;
 
-let hasClickedEasy = false;
+
 
 
 let int = document.getElementById('flips');
@@ -22,23 +23,23 @@ let medium = document.getElementById('medium');
 let startGame = document.getElementById('start-game');
 
 easy.addEventListener('click', function easy() {
-    
+
     console.log('You selected easy');
     cards.forEach(sign => sign.style.display = 'block');
 
-})
+});
 
 medium.addEventListener('click', function medium() {
-    $("#exampleModal").modal('hide');
+
     console.log('You selected medium');
     cards.forEach(sign => sign.classList.remove = 'game-card');
     cardsMd.forEach(sign => sign.setAttribute('style', 'display: block; width: calc(16.666% - 8px); height: calc(33.333% - 8px); position: relative; margin: 4px; transform: scale(1); transform-style: preserve-3d; transition: all 0.4s ease; background-color: rgba(230, 243, 223, 0.5); border-radius: 5%;'));
-})
-    
+});
+
 
 startGame.addEventListener('click', function startGame() {
     $("#exampleModal").modal('hide');
-})
+});
 
 
 
@@ -46,95 +47,108 @@ startGame.addEventListener('click', function startGame() {
 
 
 function cardFlip() {
-
-    integer += 1;
-    if (lock) return;
+    if (lock === true) return;
     if (this === firstSign) return;
-
+    integer += 1;
     this.classList.add('cardFlipped');
 
     int.innerHTML = 'Flips: ' + integer;
 
-    // first click
+    
     if (!hasFlipped) {
+        // first click
         hasFlipped = true;
         firstSign = this;
-        
-    // second click    
-    } else {
-        
-        secondSign = this;
 
-        if (firstSign.dataset.name === secondSign.dataset.name) {
+        return;      
+    } 
+    // second click 
+    secondSign = this;    
+
+    checkMatching();
+}
+
+
+function checkMatching() {
+    if (firstSign.dataset.name === secondSign.dataset.name) {
             //It's a match
-            firstSign.removeEventListener('click', cardFlip);
-            secondSign.removeEventListener('click', cardFlip);
+            disableFlip();
             setTimeout(function () {
                 firstSign.style.transform = "rotateY(180deg)";
                 secondSign.style.transform = "rotateY(180deg)";
             }, 300);
 
-            reset();
             
+
         } else {
-          // Not a match  
-          lock = true;
-          setTimeout(function() {
-            firstSign.classList.remove('cardFlipped');
-            secondSign.classList.remove('cardFlipped');
+            // Not a match  
             
-            reset();
-          }, 1000);
-            
-        }    
-    }
+            notMatching();
+        }
 }
 
+function disableFlip() {
+    firstSign.removeEventListener('click', cardFlip);
+    secondSign.removeEventListener('click', cardFlip);
 
-function cardFlipMd() {
-
-    integer += 1;
-    if (lock) return;
-    if (this === firstSign) return;
-
-    this.classList.add('cardFlipped');
-
-    int.innerHTML = 'Flips: ' + integer;
-
-    // first click
-    if (!hasFlipped) {
-        hasFlipped = true;
-        firstSign = this;
-        
-    // second click    
-    } else {
-        
-        secondSign = this;
-
-        if (firstSign.dataset.name === secondSign.dataset.name) {
-            //It's a match
-            firstSign.removeEventListener('click', cardFlipMd);
-            secondSign.removeEventListener('click', cardFlipMd);
-            setTimeout(function () {
-                firstSign.style.transform = "rotateY(180deg)";
-                secondSign.style.transform = "rotateY(180deg)";
-            }, 300);
-
-            reset();
-            
-        } else {
-          // Not a match  
-          lock = true;
-          setTimeout(function() {
-            firstSign.classList.remove('cardFlipped');
-            secondSign.classList.remove('cardFlipped');
-            
-            reset();
-          }, 1000);
-            
-        }    
-    }
+    reset();
 }
+
+function notMatching() {
+    lock = true;
+
+    setTimeout(function () {
+                firstSign.classList.remove('cardFlipped');
+                secondSign.classList.remove('cardFlipped');
+
+                reset();
+            }, 1000);
+}
+
+// function cardFlipMd() {
+
+//     integer += 1;
+//     if (lock) return;
+//     if (this === firstSign) return;
+
+//     this.classList.add('cardFlipped');
+
+//     int.innerHTML = 'Flips: ' + integer;
+
+//     // first click
+//     if (!hasFlipped) {
+//         hasFlipped = true;
+//         firstSign = this;
+
+//         // second click    
+//     } else {
+
+//         secondSign = this;
+
+//         if (firstSign.dataset.name === secondSign.dataset.name) {
+//             //It's a match
+//             firstSign.removeEventListener('click', cardFlipMd);
+//             secondSign.removeEventListener('click', cardFlipMd);
+//             setTimeout(function () {
+//                 firstSign.style.transform = "rotateY(180deg)";
+//                 secondSign.style.transform = "rotateY(180deg)";
+//             }, 300);
+
+//             reset();
+
+//         } else {
+//             // Not a match  
+//             lock = true;
+//             setTimeout(function () {
+//                 firstSign.classList.remove('cardFlipped');
+//                 secondSign.classList.remove('cardFlipped');
+
+//                 reset();
+//             }, 1000);
+
+//         }
+//     }
+// }
 
 
 
@@ -147,19 +161,13 @@ function cardFlipMd() {
 })();
 
 function reset() {
-    [hasFlipped, lock] = [false, false];
-    [firstSign, secondSign] = [null, null];
+    hasFlipped = false;
+    lock = false;
+    firstSign = null;
+    secondSign = null;
 }
 
 
 
 cards.forEach(board => board.addEventListener('click', cardFlip));
-cardsMd.forEach(boardMd => boardMd.addEventListener('click', cardFlipMd));
-
-
-
-
-
-
-
-
+//cardsMd.forEach(boardMd => boardMd.addEventListener('click', cardFlipMd));
