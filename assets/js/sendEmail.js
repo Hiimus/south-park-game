dcd85d310a2d247ffccda2527d959732028bd948
Hiimus <hiimhanseneirik@gmail.com>
2021-06-17 14:25:52 +0000

    Added feedback when email is sent from contact.html

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,5 +1,8 @@
-//Email service provided from emailjs
+let message = document.getElementById("email-message");
 
+
+
+//Email service provided from emailjs
 function sendMail(contactForm) {
     emailjs.send("service_dt2dr7c", "south_park", {
         "from_name": contactForm.name.value,
@@ -9,6 +12,10 @@ function sendMail(contactForm) {
     .then(
         function (response) {
             console.log("SUCCESS", response);
+            message.classList.remove("d-none");
+            setTimeout(function (){
+                message.classList.add("d-none");
+            }, 5000);
         }, 
         function (error) {
             console.log("FAILED", error);
