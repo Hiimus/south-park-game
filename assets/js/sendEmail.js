c5530f86bf1ff1efadd9987f4496e9810fe9f113
Hiimus <hiimhanseneirik@gmail.com>
2021-06-23 13:32:00 +0000

    Added a function that clears the form when submitting

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -4,11 +4,13 @@ let message = document.getElementById("email-message");
 
 //Email service provided from emailjs
 function sendMail(contactForm) {
+    clearText();
     emailjs.send("service_dt2dr7c", "south_park", {
         "from_name": contactForm.name.value,
         "from_email": contactForm.emailaddress.value,
         "project_request": contactForm.message.value
     })
+    
     .then(
         function () {
             message.classList.remove("d-none");
@@ -16,7 +18,17 @@ function sendMail(contactForm) {
                 message.classList.add("d-none");
             }, 5000);
         } 
+        
     );
     return false; // To block from loading a new page
+    
+}
+
+/*This function clears the text when clicking on submit button*/
+
+function clearText() {
+    document.getElementById('fullname').value='';
+    document.getElementById('emailaddress').value='';
+    document.getElementById('message').value='';
 }
 
